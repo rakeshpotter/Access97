81ed58543642ebbbfbb0fbc39967f04926cb71d8
rakeshpotter <rakeshpatrachar@gmail.com>
2016-07-15 09:56:55 -0400

    Session Checking Error Fixed.

--- __sessionChecking.php
+++ __sessionChecking.php
@@ -3,7 +3,7 @@
 session_start();
 define("PASSWORD", $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
 
-if (isset($_SESSION['access97']) && $_SESSION['access97'] == PASSWORD) {
+if (isset($_SESSION['access97'])) {
     echo "<a href='logout.php' style='float:right;'>Logout</a>";
 } else {
     $_SESSION['dbname'] = NULL;
