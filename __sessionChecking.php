3e121102ec7214b5f2f41a151a6a3c41d1288946
rakeshpotter <rakeshpatrachar@gmail.com>
2016-07-27 04:50:45 -0400

    Super User Security Added.

--- __sessionChecking.php
+++ __sessionChecking.php
@@ -1,10 +1,17 @@
 <?php
 
 session_start();
-define("PASSWORD", $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
 
+define("USERNAME", "1718d57986110b6af2dd96b59d3db416");
+
+$superUser = '';
 if (isset($_SESSION['access97'])) {
-    echo "<a href='logout.php' style='float:right;'>Logout</a>";
+    if (USERNAME == md5($_SESSION['username'])) {
+        $superUser = " Super User";
+    }
+    if ($_SERVER['SCRIPT_NAME'] == "/Access97/index.php") {
+        echo "<a href='logout.php' style='float:right;'>Logout$superUser</a>";
+    }
 } else {
     $_SESSION['dbname'] = NULL;
 }
